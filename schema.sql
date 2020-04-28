97a09084bbec272987be438e6a26a66c1ed436b3
CJPALME <cjpalmerin@gmail.com>
2020-04-27 20:47:30 -0500

    Added schema.sql with three tables.

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id DECIMAL,
+  manager_id INT
+  PRIMARY KEY (id)
+)
